85a60989da7119c9cc78c47462f655bd214c09df
Juan Camilo Cespedes Romero <juankamilocromero@gmail.com>
2023-05-25 11:24:58 -0500

    Se agregaron algunos endpoint con funciones y ya la BD corre en la nube

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import psycopg2
 from flask import Flask
 from flask import jsonify
+from flask import request
 import json
 
-
+from flask_cors import CORS
 
 app=Flask(__name__)
-
+cors = CORS(app)
 def loadFileConfig():
     with open('config.json') as f:
         data = json.load(f)
@@ -19,10 +20,9 @@ def conectarBD():
 def cerrarBD(DBconection):
     DBconection.close()
 
-def consultaSelect(infoAimprimir,tablas):
-    #en infoAimprimir agregar * o la informacion que se desea realizar
-    #agregar a tablas tod lo que se escriba despues del from
-    return  "SELECT "+infoAimprimir+" FROM PARQUEADERO."+tablas
+def guardarCambiosEnBD(curs):
+    curs.execute("COMMIT")
+
 
 
 @app.route("/",methods=['GET'])
@@ -48,7 +48,7 @@ def get_clienteByid(cliente_id):
         cursor = DBconn.cursor()
 
         # Ejecutar el procedimiento almacenado
-        # cursor.callproc('nombre_procedimiento', parametros)
+        #cursor.callproc('', parametros)
         print("SELECT * FROM PARQUEADERO.CLIENTE")
         cursor.execute("SELECT * FROM PARQUEADERO.CLIENTE WHERE K_CLIENTE = "+str(cliente_id))
 
@@ -66,11 +66,88 @@ def get_clienteByid(cliente_id):
         return jsonify({'error': str(e)}), 500
 
 
-   #todo El de agregar cliente se hace con un callproc de la BD que no esta aun definida.
+@app.route("/cliente/<string:emailCliente>",methods=['POST'])
+def post_newClaveCliente(emailCliente):
+    try:
+        # Obtener los parámetros del cuerpo de la solicitud
+        # info_result = request.get_json()
+        # todo eliminar este diccionario y habilitar el info_result
+        info_result = {
+            "clave_nueva": "3123566333",
+        }
+        # parametros = request.json.get('parametros')
+
+        # Conectarse a la base de datos PostgreSQL
+        DBconn = conectarBD()
+
+        # Crear un cursor
+        cursor = DBconn.cursor()
+        # Parametros del procedimiento o funcion
+        par = (emailCliente, info_result["clave_nueva"])
+        # Ejecutar el procedimiento almacenado
+        cursor.callproc('PARQUEADERO.CAMBIO_CLAVE_USUARIO_PR', par)
+        #Commit en BD
+        guardarCambiosEnBD()
+        # Recuperar los resultados, si los hay
+        results = cursor.fetchall()
+
+        # Cerrar el cursor y la conexión
+        cursor.close()
+        cerrarBD(DBconn)
+
+        # Devolver los resultados como respuesta en formato JSON
+        return jsonify(results)
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+@app.route("/cliente",methods=['POST'])
+def add_cliente():
+    try:
+        # Obtener los parámetros del cuerpo de la solicitud
+        #info_result = request.get_json()
+        #todo eliminar este diccionario y habilitar el info_result
+        info_result = {
+            "tipo_id": "CC",
+            "num_id" : "1013680174",
+            "f_nom" : "Juan",
+            "s_nom" : "Camilo",
+            "f_apell" : "Cespedes",
+            "s_apell" : "Romero",
+            "tel" : "3123566333",
+            "email" : "juankamilocromero@gmail.com"
+        }
+        # parametros = request.json.get('parametros')
+
+        # Conectarse a la base de datos PostgreSQL
+        DBconn = conectarBD()
+
+        # Crear un cursor
+        cursor = DBconn.cursor()
+        #Parametros del procedimiento o funcion
+        par = (info_result["tipo_id"],info_result["num_id"],info_result["f_nom"],info_result["s_nom"],info_result["f_apell"],info_result["s_apell"],info_result["tel"],info_result["email"])
+        # Ejecutar el procedimiento almacenado
+        cursor.callproc('PARQUEADERO.CREAR_CLIENTE_FU', par)
+
+
+        # Recuperar los resultados, si los hay
+        results = cursor.fetchall()
+
+        # Cerrar el cursor y la conexión
+        cursor.close()
+        cerrarBD(DBconn)
+
+        # Devolver los resultados como respuesta en formato JSON
+        return jsonify(results)
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
 
 """
 ---------------------------
-    ENDPOINTS CLIENTE PARQUEADEROS
+    ENDPOINTS PARQUEADEROS
 --------------------------
 """
 
@@ -87,9 +164,9 @@ def get_parqueaderos():
         cursor = DBconn.cursor()
 
         # Ejecutar el procedimiento almacenado
-        #cursor.callproc('nombre_procedimiento', parametros)
-        print("SELECT * FROM PARQUEADERO.SUCURSAL")
-        cursor.execute("SELECT * FROM PARQUEADERO.SUCURSAL")
+        cursor.callproc('PARQUEADERO.MOSTRAR_SUCURSALES_FU', ())
+        #print("SELECT * FROM PARQUEADERO.SUCURSAL")
+        #cursor.execute("SELECT * FROM PARQUEADERO.SUCURSAL")
 
         # Recuperar los resultados, si los hay
         results = cursor.fetchall()
@@ -114,7 +191,7 @@ def get_parqueaderos():
 """
 
 
-@app.route("/cliente/vehiculos/marcas>", methods=["GET"])
+@app.route("/cliente/vehiculos/marcas", methods=["GET"])
 def get_marcas():
     try:
         # Obtener los parámetros del cuerpo de la solicitud
