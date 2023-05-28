1681d6fe1ab30cdc96a7579345630ab990f8aa5d
Andrés Beltrán <andres.beltran@citi.com.mx>
2023-05-28 15:28:07 -0500

    corrección blueprints

--- main.py
+++ main.py
@@ -7,13 +7,14 @@ import json
 from flask_cors import CORS
 
 from functions_db import loadFileConfig
+from routes.parking import routes_parking
 from routes.users import routes_user
 
 app = Flask(__name__)
 cors = CORS(app)
 
-app.register_blueprint(routes_user, url_prefix="/api")
-
+app.register_blueprint(routes_user)
+app.register_blueprint(routes_parking)
 
 @app.route("/", methods=['GET'])
 def test():
